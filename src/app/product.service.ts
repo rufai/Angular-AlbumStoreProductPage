59aa9c618ae978d51ce9bd69a6670bfc7080dc09
howtobuildx <htbuildx@gmail.com>
2020-09-30 15:11:22 +0100

    update file product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http'
-import 'rxjs/add/operator/map'
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-	
-	private _albumUrl: string = "../assets/album.json"
 
-  	constructor(private _http: Http) {
-  
-   	}
+  private _albumUrl = '../assets/album.json';
 
-    getAlbum( id: number ) {
-    	// console.log("in get album")
-    	// console.log(this._http.get(this._albumUrl))
-  	 	return this._http.get(this._albumUrl)
-  	 		.map(response => response.json())
-   }
+  constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
+  }
 }
