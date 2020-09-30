6018ccf05b65766562e3d53df98e66cdf3c88419
howtobuildx <htbuildx@gmail.com>
2020-09-30 17:29:32 +0100

    added central routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes  } from '@angular/router';
+
 import { AppComponent  } from './app.component';
 import { ProductService  } from './product.service';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+
+const appRoutes: Routes = [
+  {
+    "path": "products",
+    "component":ProductListComponent
+  },
+  {
+    "path": "products/:id",
+    "component":ProductPageComponent
+  },
+  {
+    "path": "",
+    "redirectTo": "products",
+    "pathMatch": "full"
+  }
+]
 
 @NgModule({
   declarations: [
@@ -21,7 +39,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
